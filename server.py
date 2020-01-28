5c31087ec762c2ad06d2b5ced27e5943b5b98cd7
mike32n <mihaly32horvath@gmail.com>
2020-01-28 13:38:57 +0100

    creating routes, index and list html templates

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route("/list")
+def list():
+    return render_template("list.html")
 
 
 if __name__ == "__main__":
