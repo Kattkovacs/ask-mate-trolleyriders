d7f8037ef9cd1e59eea88b409ec03d1a8a78810e
mike32n <mihaly32horvath@gmail.com>
2020-01-28 19:47:55 +0100

    decoding timestamp

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import csv
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 dirname = os.path.dirname(__file__)
@@ -11,11 +12,13 @@ def hello():
     return render_template("index.html")
 
 @app.route("/list")
-def list(file=f"{dirname}/sample_data/question.csv"):
-    with open(file) as q:
+def list():
+    with open(f"{dirname}/sample_data/question.csv") as q:
         questions = [{k: v for k, v in row.items()} for row in csv.DictReader(q, skipinitialspace=True)]
+        for item in questions:
+            item['submission_time'] = datetime.fromtimestamp(int(item['submission_time']))
     return render_template("list.html", questions=questions)
-
+    # sorted(x.items(), key=lambda item: item[1])
 
 if __name__ == "__main__":
     app.run()
