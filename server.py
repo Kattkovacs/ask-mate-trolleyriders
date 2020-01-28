360b478db9f05ff43cc04ab1376bdf6cd1f20814
mike32n <mihaly32horvath@gmail.com>
2020-01-28 14:38:53 +0100

    printing list of dictionaries

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+import csv
+import os
 
 app = Flask(__name__)
+dirname = os.path.dirname(__file__)
 
 
 @app.route("/")
@@ -8,8 +11,10 @@ def hello():
     return render_template("index.html")
 
 @app.route("/list")
-def list():
-    return render_template("list.html")
+def list(file=f"{dirname}/sample_data/question.csv"):
+    with open(file) as q:
+        questions = [{k: v for k, v in row.items()} for row in csv.DictReader(q, skipinitialspace=True)]
+    return render_template("list.html", questions=questions)
 
 
 if __name__ == "__main__":
