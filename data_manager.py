4dddb143cc0bd0fd4f32d4d118209914db073865
mike32n <mihaly32horvath@gmail.com>
2020-01-29 10:10:20 +0100

    decode_timestamp in data_manager.py

---
+++ data_manager.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+def decode_timestamp(list):
+    for item in list:
+        item['submission_time'] = datetime.fromtimestamp(int(item['submission_time']))
+    return list
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import csv
 import os
-from datetime import datetime
+import data_manager
 
 app = Flask(__name__)
 dirname = os.path.dirname(__file__)
@@ -16,8 +16,7 @@ def hello():
 def list():
     with open(f"{dirname}/sample_data/question.csv") as q:
         questions = [{k: v for k, v in row.items()} for row in csv.DictReader(q, skipinitialspace=True)]
-        for item in questions:
-            item['submission_time'] = datetime.fromtimestamp(int(item['submission_time']))
+    questions = data_manager.decode_timestamp(questions)
     return render_template("list.html", questions=questions)
 
 
@@ -25,19 +24,19 @@ def list():
 def question(question_id):
     with open(f"{dirname}/sample_data/question.csv") as q:
         questions = [{kq: vq for kq, vq in rowq.items()} for rowq in csv.DictReader(q, skipinitialspace=True)]
-        quest =  []
-        for item in questions:
-            item['submission_time'] = datetime.fromtimestamp(int(item['submission_time']))
-            if item['id'] == question_id:
-                quest.append(item)
+    quest =  []
+    questions = data_manager.decode_timestamp(questions)
+    for item in questions:
+        if item['id'] == question_id:
+            quest.append(item)
 
     with open(f"{dirname}/sample_data/answer.csv") as a:
         answers = [{ka: va for ka, va in rowa.items()} for rowa in csv.DictReader(a, skipinitialspace=True)]
-        answ = []
-        for item in answers:
-            item['submission_time'] = datetime.fromtimestamp(int(item['submission_time']))
-            if item['question_id'] == question_id:
-                answ.append(item)
+    answ = []
+    answers = data_manager.decode_timestamp(answers)
+    for item in answers:
+        if item['question_id'] == question_id:
+            answ.append(item)
     return render_template("question.html", quest=quest, answ=answ)
 
 
